3bef9b55c5f37b1a386c4848ecbb13f2fd541874
Dexter43 <svalerian.94@gmail.com>
2018-09-19 12:45:09 +0200

    5

---
+++ src/com/company/Pares.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Pares {
+    public static void main(String[] args){
+
+        for (int i = 1; i <= 10; i++) {
+
+            if (i % 2 == 0){
+                System.out.print(i + " ");
+
+            }
+
+
+        }
+    }
+}
