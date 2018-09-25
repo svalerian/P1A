2e3605be5e3b5c63449b2f8683efb21696fde5fa
Dexter43 <svalerian.94@gmail.com>
2018-09-25 21:15:38 +0200

    cambios

--- src/com/company/Division.java
+++ src/com/company/Division.java
@@ -3,7 +3,13 @@ package com.company;
 public class Division {
     public static void main(String[] args){
         for (int i = 1; i <= 15; i++) {
+            if (i % 2 == 0){
+                System.out.println(i + " ");
 
+            }
+           // else {
+            //    System.out.println(i + " ");
+           // }
         }
     }
 }
---
+++ src/com/company/Switch.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Switch {
+    public static void main(String[] args){
+        int dia=31;
+        switch (dia){
+            case 1: case 8: case 15:
+        }
+    }
+}
